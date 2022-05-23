7ec68234e3dab5f82619ea86d931ce2d89a4490a
Sacha LAHLOU <slahlou@e3r11p19.clusters.42paris.fr>
2022-05-23 18:53:02 +0200

    jsp

--- test.c
+++ test.c
@@ -1,4 +1,6 @@
+
 #include <sys/types.h> //pour le type pid_t
+#include <sys/wait.h> //pour la fonction wait
 #include <unistd.h> //pour fork
 #include <stdio.h>
 
@@ -22,15 +24,70 @@
 //     return (0);
 // }
 
-int main (void)
+// int main (void)
+// {
+//     int i, n = 5;
+//     int child_pid;
+
+//     for (i = 1; i < n; i++)
+//     {
+//         if ((child_pid = fork()) <= 0 ) break;
+//             printf("Processus en cour = %d, processus parent = %d, i = %d\n", getpid(), getppid(), i);
+//     }
+//     return (0);
+// }
+
+// int main(void)
+// {
+//     int i, n = 5;
+//     pid_t pid;
+//     int test = getpid();
+
+//     printf("\n\t\t AVANT LE FORKS \n\t\tprocessus courant = %d, processus parent = %d\n\n", getpid(), test);
+
+//     for (i = 1; i < n; i++)
+//     {
+//         if ((pid = fork()) == -1) break;
+
+//         if (pid == 0)
+//         {
+//             if (getppid() == test)
+//                 printf("FIRST LAYER-->  ");
+//             printf("processus courant = %d, processus pere = %d, i = %d\n", getpid(), getppid(), i);
+//         }
+        
+//     }
+//     return (0);
+// }
+
+
+// int main(void)
+// {
+//     pid_t pid = 0;
+
+//     printf("processus courant = %d, processus pere = %d\n", getpid(), getppid());
+
+//     pid = fork();
+//     printf("processus courant = %d, processus pere = %d\n", getpid(), getppid());
+    
+//     return (0);
+// }
+
+int main(void)
 {
     int i, n = 5;
-    int child_pid;
+    pid_t pid = 0;
+    int test = getpid();
+
+    printf("\n\t\t AVANT LE FORKS \n\t\tprocessus courant = %d, processus parent = %d\n\n", test, getppid());
 
     for (i = 1; i < n; i++)
     {
-        if ((child_pid = fork()) <= 0 ) break;
-            printf("Processus en cour = %d, processus parent = %d, i = %d\n", getpid(), getppid(), i);
+        if ((pid = fork()) == -1) break ;
+        if (pid == 0)
+            printf("Processus courant = %d et processus pere = %d, i = %d\n\n", getpid(), getppid(), i);
     }
+    while (wait(NULL) >= 0);
+
     return (0);
 }
