50b125bd698ff81e33a67c6eef383fc543d6c7db
Sacha LAHLOU <slahlou@e3r10p20.clusters.42paris.fr>
2022-05-24 13:27:31 +0200

    test

--- test.c
+++ test.c
@@ -3,6 +3,7 @@
 #include <sys/wait.h> //pour la fonction wait
 #include <unistd.h> //pour fork
 #include <stdio.h>
+#include <stdlib.h>
 
 // int main(void)
 // {
@@ -73,21 +74,48 @@
 //     return (0);
 // }
 
-int main(void)
+// int main(void)
+// {
+//     int i, n = 5;
+//     pid_t pid = 0;
+//     int test = getpid();
+
+//     printf("\n\t\t AVANT LE FORKS \n\t\tprocessus courant = %d, processus parent = %d\n\n", test, getppid());
+
+//     for (i = 1; i < n; i++)
+//     {
+//         if ((pid = fork()) == -1) break ;
+//         if (pid == 0)
+//             printf("Processus courant = %d et processus pere = %d, i = %d\n\n", getpid(), getppid(), i);
+//     }
+//     while (wait(NULL) >= 0);
+
+//     return (0);
+// }
+
+void fils(int i)
 {
-    int i, n = 5;
-    pid_t pid = 0;
-    int test = getpid();
+    printf("\n\n\tfils %d en cours de destruction...\n", i);
+    sleep(2);
+    exit(i);
+}
 
-    printf("\n\t\t AVANT LE FORKS \n\t\tprocessus courant = %d, processus parent = %d\n\n", test, getppid());
+int main (void)
+{
+    int status;
+    printf("\n\t\tprocessus courant = %d, processus parent = %d\n\n", getpid(), getppid());
 
-    for (i = 1; i < n; i++)
+    if (fork())
     {
-        if ((pid = fork()) == -1) break ;
-        if (pid == 0)
-            printf("Processus courant = %d et processus pere = %d, i = %d\n\n", getpid(), getppid(), i);
+        if (fork() == 0)
+            fils(2);
     }
-    while (wait(NULL) >= 0);
-
+    else
+        fils (1);
+    printf("\n\t\tprocessus courant = %d, processus parent = %d\n\n", getpid(), getppid());
+    if (wait(&status) > 0)
+         printf("fin du fils %d\n", status >> 8);
+    if (wait(&status) > 0)
+        printf("fin du fils %d\n", status >> 8);
     return (0);
 }
